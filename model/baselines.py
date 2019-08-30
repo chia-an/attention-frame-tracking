6895bdcafd4939671830cc7b259747666035d4d4
chia-an <chia-an.yu@epfl.ch>
2019-08-30 02:14:25 +0200

    Clean up baselines.py

--- model/baselines.py
+++ model/baselines.py
@@ -14,16 +14,10 @@ from utils.parse_multiwoz import MultiwozDataset, \
 
 
 def main():
-    dials = []
-    for fold in range(1, 11):
-        with open(str(frames_data_dir / '{}.json'.format(fold)),
-                  'r') as f_data:
-            dials += json.load(f_data)
-
-    dataset = MultiwozDataset(dicts=multiwoz_load_dicts(),
-                              data_filename='valid_mixed_multiwoz.json')
-    # dataset = FramesDataset(dicts=frames_load_dicts(), folds=range(1, 11))
-    # dataset = FramesDataset(dicts=frames_load_dicts(), folds=[1])
+    dataset = FramesDataset(dicts=frames_load_dicts(),
+                            folds=[10],
+                            slot_based=True,
+                            user_only=True)
 
     preds = []
     total = 0
@@ -34,52 +28,32 @@ def main():
         n_frames = len(frames)
         preds_turn = [[0] * n_frames for _ in range(len(asvs))]
 
-        # Do something here.
-
         # Random
-        # for i, asv in enumerate(asvs):
-        #     frame = random.randrange(n_frames)
-        #     preds_turn[i][frame] = 1
-
-        # Always predict active frame (cheating)
-        # for i, asv in enumerate(asvs):
-        #     preds_turn[i][active_frame] = 1
-
-        # Perfect answer
-        # for i, asv in enumerate(asvs):
-        #     preds_turn[i][0] = 1
-        #     preds_turn[i][fs[i]] = 1
-        # act_based = [0] * n_frames
-        # for asv in asvs:
-        #     if asv[2] == 'NOANNO':
-        #         act_based[asv[0]] = 1
-        # for i, asv in enumerate(asvs):
-        #     if asv[2] == 'NOANNO':
-        #         preds_turn[i] = act_based
-        #     else:
-        #         preds_turn[i][asv[0]] = 1
-
-        # Compare sv to sv in frames
-        #   ref to the first appearance
-        #   o.w. ref to active frame
         for i, asv in enumerate(asvs):
-            frame_id = active_frame.item()
-            for f_id, frame in enumerate(frames):
-                found = False
-                for sv in frame:
-                    is_equal = asv[1] == sv[0]
-                    is_equal &= len(asv[2]) == len(sv[1])
-                    if is_equal:
-                        for w_id, w in enumerate(asv[2]):
-                            is_equal &= asv[2][w_id].tolist() == \
-                                        sv[1][w_id].tolist()
-                    if is_equal:
-                        frame_id = f_id
-                        found = True
-                        break
-                if found:
-                    break
-            preds_turn[i][frame_id] = 1
+            frame = random.randrange(n_frames)
+            preds_turn[i][frame] = 1
+
+        # # Compare sv to sv in frames
+        # #   ref to the first appearance
+        # #   o.w. ref to active frame
+        # for i, asv in enumerate(asvs):
+        #     frame_id = active_frame.item()
+        #     for f_id, frame in enumerate(frames):
+        #         found = False
+        #         for sv in frame:
+        #             is_equal = asv[1] == sv[0]
+        #             is_equal &= len(asv[2]) == len(sv[1])
+        #             if is_equal:
+        #                 for w_id, w in enumerate(asv[2]):
+        #                     is_equal &= asv[2][w_id].tolist() == \
+        #                                 sv[1][w_id].tolist()
+        #             if is_equal:
+        #                 frame_id = f_id
+        #                 found = True
+        #                 break
+        #         if found:
+        #             break
+        #     preds_turn[i][frame_id] = 1
 
         preds.append(preds_turn)
         total += fs.numel()
@@ -88,12 +62,6 @@ def main():
 
     print('{} / {} = {}'.format(correct, total, correct / total))
     return
-
-    gen_dials = preds_to_dial_json(dials, preds)
-
-    with open(str(Path(__file__).parent / \
-                  '../results/gen.json'), 'w') as f_gen:
-        json.dump(gen_dials, f_gen)
 
 
 if __name__ == '__main__':
